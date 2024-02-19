ae2a706ae7c2edfd8cd60339c48d899ca4f69935
Vishal Sugandh <thevishalsugandh@gmail.com>
2024-02-19 07:37:14 +0530

    update

---
+++ main.py
@@ -0,0 +1,8 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+
+driver = webdriver.Firefox()
+driver.get("http://judgments.ecourts.gov.in")
+input("")
+
